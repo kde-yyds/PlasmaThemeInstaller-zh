c6b01f37fd91c5fc81947a3579834c45380f051d
kde-yyds <kde-yyds@qq.com>
2022-06-05 17:08:04 +0800

    update

--- main.cpp
+++ main.cpp
@@ -10,5 +10,5 @@ void init()
 }
 void downloadlist()
 {
-    system("cd ~/.tmp/plasmathemeinstaller-zh&&git clone https://gitee.com/vmware16/chroot-box86-wine-appstore 1>/dev/null 2>/dev/null");
+    system("cd ~/.tmp/plasmathemeinstaller-zh&&git clone https://github.com/kde-yyds/PlasmaThemeInstaller-zh 1>/dev/null 2>/dev/null");
 }
